973137b776d98786fa4ddec067320d5b8d874fc6
Cristian Camargo <chris.camargo2015@gmail.com>
2021-07-12 22:43:15 -0400

    aula1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,90 @@
-import styled from 'styled-components'
+import MainGrid from '../src/componentes/MainGrid'
+import Box from '../src/componentes/Box'
+import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AluraCommons';
+import {ProfileRelationsBoxWrapper} from '../src/componentes/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+function ProfileSidebar(propriedades){
+  return (
+
+      <Box >
+        <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}}/> 
+      </Box>
+  )
+}
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  const githubUser = 'cfcamargo';
+  const pessoasFavoritas = ['juunegreiros', 
+                            'omariosouto', 
+                            'peas', 
+                            'rafaballerini',
+                            'marcobrunodev',
+                            'felipefialho']
+
+  return (
+    <>    
+
+      <AlurakutMenu />
+
+      <MainGrid>
+
+        <div className = "profileArea" style={{gridArea: 'profileArea'}}>
+          <ProfileSidebar githubUser = {githubUser}/>
+        </div>
+
+        <div className = "welcomeArea" style={{gridArea: 'welcomeArea'}}>
+          <Box>
+
+            <h1 className = "title">
+              Bem vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+
+          </Box>
+        </div>
+
+        <div className = "profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade({pessoasFavoritas.length})
+            </h2>
+
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`}></img>
+                      <span>{itemAtual}</span>
+                    </a> 
+
+                  </li>
+
+                )
+              })}
+              </ul>
+
+          </ProfileRelationsBoxWrapper>
+          
+          <Box>
+            <h2>
+              Comunidades
+            </h2>
+              
+          </Box>
+        </div>
+
+      </MainGrid>
+
+    </>
+  )
 }
